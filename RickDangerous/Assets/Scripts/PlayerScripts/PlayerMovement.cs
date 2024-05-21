0b437b0bc15c5d7e7f18236d120279eab5d9e765
Eduardo Silveiro <202100221@estudantes.ips.pt>
2024-05-22 00:50:11 +0100

    animacoes e o crlh

--- RickDangerous/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ RickDangerous/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     [SerializeField] PlayerStatusSO playerStatus;
     private BoxCollider2D boxCollider2D;
+    private Vector2 originalColliderSize;
+    private Vector2 crouchingColliderSize;
+    private Vector2 originalColliderOffset;
+    private Vector2 crouchingColliderOffset;
     private Rigidbody2D rb;
     private float dirX;
     private char dir;
@@ -15,9 +17,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask jumpableGround;
 
     private SpriteRenderer spriteRenderer;
-
     private Animator animator;
-
 
     void Start()
     {
@@ -26,41 +26,34 @@ public class PlayerMovement : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         dir = 'R';
+
+        // Store the original size and offset of the collider
+        originalColliderSize = boxCollider2D.size;
+        originalColliderOffset = boxCollider2D.offset;
+
+        // Define the size and offset for the crouching state
+        crouchingColliderSize = new Vector2(originalColliderSize.x, originalColliderSize.y / 2);
+        crouchingColliderOffset = new Vector2(originalColliderOffset.x, originalColliderOffset.y / 2);
     }
 
-    // Update is called once per frame
     private void Update()
     {
-
         dirX = Input.GetAxisRaw("Horizontal");
-
 
         rb.velocity = new Vector2(dirX * playerStatus.Speed, rb.velocity.y);
 
-
         if (Input.GetButtonDown("Jump") && IsGrounded() == true)
         {
-
-
             rb.velocity = new Vector2(rb.velocity.x, playerStatus.JumpForce);
-            //jumpSoundEffect.Play();
         }
 
-        //fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
-
         AnimationState();
-
-
-
-
     }
 
     public void AnimationState()
     {
-
         if (dirX > 0f)
         {
-
             if (dir != 'R')
             {
                 transform.rotation = Quaternion.Euler(0, 0, 0);
@@ -70,7 +63,6 @@ public class PlayerMovement : MonoBehaviour
         }
         else if (dirX < 0f)
         {
-
             if (dir != 'L')
             {
                 transform.rotation = Quaternion.Euler(0, 180, 0);
@@ -82,7 +74,6 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("IsRunning", false);
         }
-
 
         if (rb.velocity.y > .1f)
         {
@@ -96,19 +87,31 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
             animator.SetBool("IsCrouching", true);
-
+            AdjustColliderForCrouch(true);
         }
         else
         {
             animator.SetBool("IsCrouching", false);
-
+            AdjustColliderForCrouch(false);
         }
+    }
 
+    private void AdjustColliderForCrouch(bool isCrouching)
+    {
+        if (isCrouching)
+        {
+            boxCollider2D.size = crouchingColliderSize;
+            boxCollider2D.offset = crouchingColliderOffset;
+        }
+        else
+        {
+            boxCollider2D.size = originalColliderSize;
+            boxCollider2D.offset = originalColliderOffset;
+        }
     }
 
     public bool IsGrounded()
     {
         return Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
-
 }

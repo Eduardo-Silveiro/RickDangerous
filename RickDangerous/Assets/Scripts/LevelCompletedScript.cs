e4832cdc8b849c300f25598a4b39e339fa2e04fa
jaime-vieira <101071059+jaime-vieira@users.noreply.github.com>
2024-06-02 21:49:56 +0100

    Score do level completed a funcionar

--- RickDangerous/Assets/Scripts/LevelCompletedScript.cs
+++ RickDangerous/Assets/Scripts/LevelCompletedScript.cs
@@ -2,16 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelCompletedScript : MonoBehaviour
 {
     [SerializeField] private PlayerStatusSO playerData;
     [SerializeField] private TextMeshProUGUI scoresText;
+    private ScoreManager scoreManager = new ScoreManager();
+    private int score;
 
     private void Start()
     {
         scoresText.text = "Score: " + playerData.Score;
+        score = playerData.Score;
         playerData.ResetAmmo();
         playerData.ResetHealth();
+    }
+
+    public void AddPlayer(GameObject gameObject)
+    {
+        string name = gameObject.GetComponent<Text>().text;
+
+        //UpdatePlayerScore(name, playerData.Score);
+
+        scoreManager.AddOrUpdateScore(name, score);
+
+        MainMenu();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
